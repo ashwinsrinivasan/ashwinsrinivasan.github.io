00c2146d57953025b0a1caac5ed10de5d83d9fa2
ashwinsrinivasan <ashwinmahadev@gmail.com>
2015-01-22 14:37:26 +0530

    Fifth

---
+++ index_files/C2VKySfB4vq.js
@@ -0,0 +1,6 @@
+/*!CK:1625202468!*//*1408951934,*/
+
+if (self.CavalryLogger) { CavalryLogger.start_js(["ZG4F0"]); }
+
+__d("ArticleChainingActionLink",["CSS","DOM","Event","csx","tidyEvent"],function(a,b,c,d,e,f,g,h,i,j,k){var l="._5g3p",m="._5g3q",n="._5g3r",o={listenForClick:function(p){var q=h.scry(p,l),r=h.find(p,m),s=h.find(p,n);q.forEach(function(t){k(i.listen(t,'click',function(){r&&g.hide(r);s&&g.show(s);}));});}};e.exports=o;},null);
+__d("XPagesFeedPostMutationControllerURIBuilder",["XControllerURIBuilder"],function(a,b,c,d,e,f){e.exports=b("XControllerURIBuilder").create("\/pages_to_watch\/feed_mutation\/",{});},null);
