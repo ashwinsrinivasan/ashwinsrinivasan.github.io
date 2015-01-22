04d462256b6a2669c1d9be87a31f94c4d952147c
ashwinsrinivasan <ashwinmahadev@gmail.com>
2015-01-22 16:00:51 +0530

    bootstraplaterst

--- index_files/0RXYH5KmGHI.js
+++
@@ -1,5 +0,0 @@
-/*!CK:297082651!*//*1421682267,*/
-
-if (self.CavalryLogger) { CavalryLogger.start_js(["v6YPj"]); }
-
-__d("tickerPhoteSnowLiftOpenStatus",["ArbiterMixin","copyProperties"],function(a,b,c,d,e,f,g,h){var i='CheckIsOpen',j={registerOpenChecker:function(k){return j.subscribe(i,function(l,m){if(k())m.is_Open=true;});},checkIsOpen:function(){var k={is_Open:false};j.inform(i,k);return k.is_Open;}};h(j,g);e.exports=j;},null);
